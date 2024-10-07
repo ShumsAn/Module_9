351072e0398a8178b0ca7886194c30da4b7d0f3d
ShumsAn <shum-sol@outlook.com>
2024-10-07 15:33:07 +0300

    "Генераторы"
    Цель: более глубоко понять особенности работы с функциями генераторами и оператором yield в Python.

---
+++ module_9_6.py
@@ -0,0 +1,10 @@
+def all_variants(text):
+    for size in range(len(text)):
+        for value in range(len(text)-size):
+            yield text[value:value+size+1]
+
+
+a = all_variants("abc")
+
+for i in a:
+    print(i)
